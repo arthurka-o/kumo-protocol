684da0833c11e6d64ced4211769fb47f7a3f2a74
Arthur Galiullin <galiullinko@gmail.com>
2023-08-14 14:52:00 +0200

    Comment out CommunityIssues

--- packages/contracts/contracts/Interfaces/ICommunityIssuance.sol
+++ packages/contracts/contracts/Interfaces/ICommunityIssuance.sol
@@ -3,17 +3,12 @@
 pragma solidity 0.8.11;
 
 interface ICommunityIssuance {
-    // --- Events ---
-
-    event KUMOTokenAddressSet(address _kumoTokenAddress);
-    event StabilityPoolFactoryAddressSet(address _stabilityPoolFactoryAddress);
-    event TotalKUMOIssuedUpdated(uint256 _totalKUMOIssued);
-
-    // --- Functions ---
-
-    function setAddresses(address _kumoTokenAddress, address _stabilityPoolAddress) external;
-
-    function issueKUMO() external returns (uint256);
-
-    function sendKUMO(address _account, uint256 _KUMOamount) external;
+    // // --- Events ---
+    // event KUMOTokenAddressSet(address _kumoTokenAddress);
+    // event StabilityPoolFactoryAddressSet(address _stabilityPoolFactoryAddress);
+    // event TotalKUMOIssuedUpdated(uint256 _totalKUMOIssued);
+    // // --- Functions ---
+    // function setAddresses(address _kumoTokenAddress, address _stabilityPoolAddress) external;
+    // function issueKUMO() external returns (uint256);
+    // function sendKUMO(address _account, uint256 _KUMOamount) external;
 }
